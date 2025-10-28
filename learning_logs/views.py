99b6b466f1635349b88b5b3c2be5b0aab9a6223d
william conner shipe <wcshipe@gmail.com>
2025-10-28 14:25:26 -0500

    1) learning_logs/views.py has index and topic functions
    2) Added path in learning_logs/urls.py
    
    3) Added base.html and topics.html in template/learning_logs/

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .models import Topic
 # Create your views here.
 #
 #
@@ -7,3 +8,11 @@ from django.shortcuts import render
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+    
