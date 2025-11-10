843c8b7f514959c796910e98b462db0ce4b6c24c
Wshipe <wcshipe@gmail.com>
2025-11-10 16:51:43 -0600

    1) Added Third party app bootstrap in settings 2) Modified base.html and index.html 3) Added restrictions and limitations for users

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     # My apps.
     'learning_logs',
     'users',
+    
+    # Third-party apps.
+    'bootstrap4',
 
     # Default django apps.
     'django.contrib.admin',
@@ -125,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_URL = 'users:login'
